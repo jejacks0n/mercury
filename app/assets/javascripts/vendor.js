7a20a675d77905d2977859f8065c94cf23120e0b
Jeremy Jackson <jejacks0n@gmail.com>
2011-05-20 19:12:55 -0600

    adjusted the vendor js to include the proper files

--- app/assets/javascripts/vendor.js
+++ app/assets/javascripts/vendor.js
@@ -1,6 +1,7 @@
 /*
  *= require jquery-1.6
- *= require jquery-ui-1.8.12.custom.min
+ *= require jquery-ui-1.8.13.custom.min
+ *= require jquery-ui-1.8.13.sortable.custom
  *= require jquery.cookie
  *= require jquery.easing
  *= require jquery.json2

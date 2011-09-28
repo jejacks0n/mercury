d93525e0b428ffa5956f3136decdd14a2e532fc7
jejacks0n <jejacks0n@gmail.com>
2011-09-27 19:09:08 -0600

    updating the json in the save dialog to look nicer

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -2,9 +2,10 @@ if (top.Mercury) {
   window.Mercury = top.Mercury;
   Mercury.PageEditor.prototype.save = function() {
     var data = this.serialize();
-    var lightview = Mercury.lightview(null, {title: 'Saving'});
+    var lightview = Mercury.lightview(null, {title: 'Saving', closeButton: true});
     setTimeout(function() {
-      lightview.loadContent('<div style="width:500px">Saving in the demo is disabled, but you can see what would be saved below.<textarea style="width:100%;height:300px">' + top.jQuery.toJSON(data) + '</textarea></div>');
+      var textarea = '<textarea style="width:100%;height:300px" wrap="off">' + top.JSON.stringify(data, null, '  ') + '</textarea>';
+      lightview.loadContent('<div style="width:500px">Saving in the demo is disabled, but you can see what would be sent to the server below.' + textarea + '</div>');
     }, 500);
   }
 }

4e81efbd65c827c87ddb42e10b52da7fd2bcc742
docwhat <docwhat@gerf.org>
2011-11-11 21:13:33 -0700

    adjustments to the engine to include mercury in precompiled assets (in all environments).  fixes #73

--- config/engine.rb
+++ config/engine.rb
@@ -2,17 +2,24 @@ require "rails"
 
 module Mercury
   class Engine < Rails::Engine
-    
-    # To load the routes for this Engine, within your main apps routes.rb file include
-    # the following:
+
+    # Additional application configuration to include precompiled assets.
+    initializer :assets do |config|
+      Rails.application.config.assets.precompile += %w( mercury.js mercury.css mercury_overrides.css )
+    end
+
+    # To load the routes for this Engine, within your main apps routes.rb file include:
     #
-    #   Mercury::Engine.routes
+    # Mercury::Engine.routes
+    #
     def self.routes
       Rails.application.routes.draw do
         match '/editor(/*requested_uri)' => "mercury#edit", :as => :mercury_editor
+
         namespace :mercury do
           resources :images
         end
+
         scope '/mercury' do
           match ':type/:resource' => "mercury#resource"
           match 'snippets/:name/options' => "mercury#snippet_options"
@@ -24,5 +31,6 @@ module Mercury
         end
       end
     end
+
   end
 end

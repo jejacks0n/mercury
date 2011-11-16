23a5455f6f1e2a43b637c1f4bd6e5db4b9bc41f9
jejacks0n <jejacks0n@gmail.com>
2011-11-15 17:59:23 -0700

    updated how gemspec works -- removed jeweler, sass-rails, and json dependencies.

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'evergreen/rails'
+require 'ruby-debug'
 
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.

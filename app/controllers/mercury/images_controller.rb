047a69bd9ffd5a376d7066ecc5330d8dcc0aa0a7
Erik Petersen <erik@telmate.com>
2011-11-16 23:09:41 -0800

    fixed bug where respond_with was sending back improperly structured json

--- app/controllers/mercury/images_controller.rb
+++ app/controllers/mercury/images_controller.rb
@@ -1,19 +1,25 @@
 class Mercury::ImagesController < MercuryController
 
-  respond_to :json
-
   # POST /images.json
   def create
-    @image = Mercury::Image.new(params[:image])
+    @image = Image.new(params[:image])
     @image.save
-    respond_with @image
+    respond_to do |format|
+      format.json {
+        render :json => {:image => @image} 
+      }
+    end
   end
 
   # DELETE /images/1.json
   def destroy
-    @image = Mercury::Image.find(params[:id])
+    @image = Image.find(params[:id])
     @image.destroy
-    respond_with @image
+    respond_to do |format|
+      format.json {
+        render :json => {:image => @image} 
+      }
+    end
   end
-
+  
 end

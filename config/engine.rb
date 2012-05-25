7834e690400cf6a6d252f6f7fec1e886d97487b1
Gabe Varela <gvarela@gmail.com>
2012-05-25 11:29:21 -0600

    Precompile mercury_overrides.js. Fixes Issue #192

--- config/engine.rb
+++ config/engine.rb
@@ -5,7 +5,7 @@ module Mercury
 
     # Additional application configuration to include precompiled assets.
     initializer :assets, :group => :all do |app|
-      app.config.assets.precompile += %w( mercury.js mercury.css mercury_overrides.css )
+      app.config.assets.precompile += %w( mercury.js mercury.css mercury_overrides.css mercury_overrides.js )
     end
 
     # Require mercury authentication module and potentially other aspects later (so they can be overridden).

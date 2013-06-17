a76c87a6691dc9847a6c556a7529da9cea24284e
sandric <kyrychenkoanton@gmail.com>
2013-06-17 15:05:24 +0300

    Change install_generator.rb so other template engines could be added for Mercury layout rendering.

--- lib/generators/mercury/install/install_generator.rb
+++ lib/generators/mercury/install/install_generator.rb
@@ -10,8 +10,8 @@ module Mercury
       class_option :full, :type => :boolean, :aliases => '-g',
                    :desc => 'Full installation will install the layout and css files for easier customization.'
 
-      class_option :haml, :type => :boolean,
-                   :desc => 'Use a Haml layout template (instead of ERB)'
+      class_option :template_engine, :type => :string,
+                   :desc => 'Set preferred template engine for layout instead of ERB (haml or slim)'
 
       def copy_config
         copy_file 'app/assets/javascripts/mercury.js'
@@ -23,7 +23,7 @@ module Mercury
 
       def copy_layout_and_css_overrides
         if options[:full] || yes?("Install the layout file and CSS? [yN]")
-          layout_ext = (options[:haml]) ? 'haml' : 'erb'
+          layout_ext = options[:template_engine] || 'erb'
           copy_file "app/views/layouts/mercury.html.#{layout_ext}"
           copy_file 'app/assets/stylesheets/mercury.css'
         end

358147632a78c4d8d177fcf6ff52a382c611e71d
Wei <jrweizhang@gmail.com>
2013-10-20 23:14:46 +0800

    Compatibility of rails 4's strong parameters.

--- lib/generators/mercury/install/images/templates/ar_paperclip_image.rb
+++ lib/generators/mercury/install/images/templates/ar_paperclip_image.rb
@@ -2,8 +2,6 @@ class Mercury::Image < ActiveRecord::Base
 
   self.table_name = :mercury_images
 
-  attr_accessible :image
-
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" },
         :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
         :url => "/system/:attachment/:id/:style/:filename"
--- lib/generators/mercury/install/images/templates/images_controller.rb
+++ lib/generators/mercury/install/images/templates/images_controller.rb
@@ -4,7 +4,7 @@ class Mercury::ImagesController < MercuryController
 
   # POST /images.json
   def create
-    @image = Mercury::Image.new(params[:image])
+    @image = Mercury::Image.new(params.require(:image).permit(:image))
     @image.save
     respond_with @image
   end

e75f1c0186ee6a40a12ec3a9d70ea5a2a790b8cb
Taurus Colvin <taurus.colvin@gmail.com>
2013-11-18 23:37:38 -0500

    support POST verb on snippet routes (and )

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Mercury::Engine.routes.draw do
   get '/editor(/*requested_uri)' => "mercury#edit", :as => :mercury_editor
 
   scope '/mercury' do
-    get ':type/:resource' => "mercury#resource"
-    get 'snippets/:name/options' => "mercury#snippet_options"
-    get 'snippets/:name/preview' => "mercury#snippet_preview"
+    get   ':type/:resource',        :to => "mercury#resource"
+    match 'snippets/:name/options', :to => "mercury#snippet_options", :via => [:get, :post]
+    match 'snippets/:name/preview', :to => "mercury#snippet_preview", :via => [:get, :post]
   end
 end
